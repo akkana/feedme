c3abfc58c861aadc61aec6d29827a5fda73bfa0c
Akkana Peck <akkana@shallowsky.com>
2013-08-21 13:28:00 -0700

    Make it actually work on Android SL4A. Use BeautifulSoup instead of lxml.html (the latter isn't available for SL4A) and put in lots of extra parser error handling for the older and more brittle version of BeautifulSoup in SL4A.

--- webfetch/feedfetcher.py
+++ webfetch/feedfetcher.py
@@ -1,26 +1,35 @@
 #!/usr/bin/env python
 
 import urllib2
-import lxml.html
+
+import BeautifulSoup
+# http://www.crummy.com/software/BeautifulSoup/bs3/documentation.html
+import HTMLParser  # Needed for the exceptions used by BeautifulSoup
+
 import sys, os
+import time
 
 # Are we on Android? Make it optional, for easier testing.
 try:
     import android
+    print "Running on Android"
     is_android = True
-    droid.makeToast("Running on Android!")
-except:
-    is_android = False
-
-if is_android:
     droid = android.Android()
+    droid.makeToast("Running on Android!")
+except ImportError:
+    print "Not running on Android"
+    is_android = False
 
 def perror(s):
     if is_android:
         droid.makeToast(s)
     print s
 
-def fetch_url_to(url, outfile, return_contents=False):
+def fetch_url_to(url, outfile):
+    if os.path.exists(outfile):
+        print outfile, "already exists -- not re-fetching"
+        return
+
     print "Fetching", url, "to", outfile
 
     # Read the URL
@@ -33,9 +42,50 @@ def fetch_url_to(url, outfile, return_contents=False):
     outf.write(contents)
     outf.close()
 
-    # Return the contents if needed
-    if return_contents:
-        return contents
+    if not url.lower().endswith('.html'):
+        return
+
+    # If html, go through the contents and recursively fetch
+    # any local links.
+    try:
+        soup = BeautifulSoup.BeautifulSoup(contents)
+    except HTMLParser.HTMLParseError, e:
+        perror("Parse error on " + url + "! " + str(e))
+        print "First part of file was:", contents[:256]
+        return
+
+    def not_a_local_link(l):
+        if not link or ':' in link or '#' in link or link[0] == '/' \
+                or link.startswith('../'):
+                # I don't know why we see ../ links, but we do.
+            return True
+        return False
+
+    dirurl = os.path.dirname(url) + '/'
+    outdir = os.path.dirname(outfile)
+
+    for tag in soup.findAll('a'):
+        # BeautifulSoup doesn't support calls like 'href' in tag
+        try:
+            link = tag['href']
+        except KeyError:
+            continue
+        if not_a_local_link(link):
+            continue
+
+        fetch_url_to(dirurl + link, os.path.join(outdir, link))
+
+    for tag in soup.findAll('img'):
+        # BeautifulSoup doesn't support calls like 'href' in tag
+        #print "Checking image", tag
+        try:
+            link = tag['src']
+        except KeyError:
+            continue
+        if not_a_local_link(link):
+            #print "Not a local link:", link
+            continue
+        fetch_url_to(dirurl + link, os.path.join(outdir, link))
 
 def fetch_feed_dir(dirurl, outdir):
     '''Fetch index.html inside the given url, plus everything it points to.'''
@@ -46,29 +96,16 @@ def fetch_feed_dir(dirurl, outdir):
     print "Fetch_feed_dir", dirurl, outdir
 
     # Make sure the directory exists
-    print "Trying to create", outdir
     if not os.access(outdir, os.W_OK):
         os.makedirs(outdir, 0755)
 
     index = fetch_url_to(dirurl + 'index.html',
-                         os.path.join(outdir, 'index.html'), True)
-
-    tree = lxml.html.fromstring(index)
-    for element, attribute, link, pos in tree.iterlinks():
-        # Only fetch local URLs.
-        print "  link", link
-        if not link or ':' in link or link[0] == '/' or '#' in link \
-                or link.startswith('../'):
-                # I don't know why we see ../ links, but we do.
-            continue
-        fetch_url_to(dirurl + link, os.path.join(outdir, link), False)
+                         os.path.join(outdir, 'index.html'))
 
 def fetch_dir_recursive(urldir, outdir):
     if not urldir.endswith('/'):
         urldir += '/'
-    print urldir, "should end with a slash now"
 
-    print "Reading directory entries for", urldir
     f = urllib2.urlopen(urldir)
     dirpage = f.read()
     # dirlines = dirpage.split('\n')
@@ -77,24 +114,21 @@ def fetch_dir_recursive(urldir, outdir):
     # Parse the directory contents to get the list of feeds
     # It's in a table tag, where valid entries will look like:
     # <tr><td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td><td><a href="BBC_News_Science/">BBC_News_Science/</a></td><td align="right">28-Jun-2013 18:42  </td><td align="right">  - </td><td>&nbsp;</td></tr>
-    # for line in dirlines:
-    #     if '<img src="/icons/folder.gif"' not in line:
-    #         continue    # Not a subdirectory
-    print "Parsing links..."
     feeddirs = []
     try :
-        tree = lxml.html.fromstring(dirpage)
-        for element, attribute, link, pos in tree.iterlinks():
-            # Save only links to files, not .. or the various sort commands
-            if element.tag == 'a' and attribute == 'href' \
-                    and link[0] != '?' and link[0] != '/':
-                print "Appending", link
-                feeddirs.append(link)
-            else: print "Not appending", element, attribute, link
-
-    except Exception, e:
-        perror("Couldn't parse directory page: " + e)
+        soup = BeautifulSoup.BeautifulSoup(dirpage)
+    except HTMLParser.HTMLParseError, e:
+        perror("Couldn't parse directory page " + urldir + str(e))
+        print "Parsed HTML began:", dirpage[:256]
         return
+
+    for tag in soup.findAll('a'):
+        try:
+            link = tag['href']
+        except KeyError:
+            continue
+        if link[0] != '?' and link[0] != '/':
+            feeddirs.append(link)
 
     # now feeddirs[] should contain the subdirs we want to fetch.
     print "Will try to fetch feed dirs:", feeddirs
@@ -103,12 +137,13 @@ def fetch_dir_recursive(urldir, outdir):
     for subdir in feeddirs:
         fetch_feed_dir(urldir + subdir, os.path.join(outdir, subdir))
 
-dirdate = '06-28-Fri'
+dirdate = time.strftime("%m-%d-%a"))
 baseurl = 'http://shallowsky.com/feeds/' + dirdate
 if is_android:
     outdir = '/mnt/sdcard/external_sd/feeds/'
 else:
     outdir = '/tmp/feeds'
+outdir = os.path.join(outdir, dirdate)
 fetch_dir_recursive(baseurl, outdir)
 
 if is_android:
